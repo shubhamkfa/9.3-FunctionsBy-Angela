006300dfe41568163edb9fe0571e5b0668e32481
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-03-10 12:11:27 +0530

    Add files via upload

---
+++ 9.3 FunctionsByAngela.js
@@ -0,0 +1,24 @@
+function getMilk(bottles) {  
+    var bottleCost=1.5;
+ 
+  console.log("moveRight");
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("You will get " + bottles/bottleCost + " Bottles of Milk");
+  console.log("moveLeft");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+getMilk(1.5);
